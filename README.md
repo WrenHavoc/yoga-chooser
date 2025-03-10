18dc624ec6d5ccead040b5d0bdb602861e5498e3
WrenHavoc <wrenhavoc@proton.me>
2025-03-10 07:52:28 -0600

    added comments to main script and updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This is designed to work with linux systems but should theoretically work on mac and other unix/unix-like environments.
 Windows sytems (in theory) could run this, but it will be buggy and missing some features.
 you could modify this to work on systems other than linux. modify the lines that use os.system and that should be it.
-for example, on windows you would want to change `os.system("clear")` to`os.system("cls")`
+for example, on windows you would want to change `os.system("clear")` to`os.system("cls")` and change the format of the file paths to match the windows format.
 
 This is just a school project.
 I'm a software dev, not a gym teacher so the information this software provides could easily be wrong.
@@ -16,11 +16,14 @@ I am considering making a full release, but I'd really only do that if I have no
 If you (for some reason) want to pick up development, please, feel free to message me over XMPP.
 This is an XMPP address, not an email address. If you try to email it, nothing will happen: wren@404.city
 
+you can add new poses by editing the files in the poses folder.
+Just add a new item to the list and it should work automatically.
+
 this is the barebones, basic version of the program. It has gone through alpha testing but not beta testing. 
 
 (possible) future features:
   - full support for windows and mac
-  - ability to add new poses
+  - ability to add new poses using tui
   - create full routine
   - built in explanation of poses
   - improved tui or possibly gui
--- yogachooser.py
+++ yogachooser.py
@@ -1,38 +1,49 @@
+# Yoga class project
+
+# Imports
 import sys, os, random
 from os import system
 
-list_arm = open("./poses/arms.txt").readlines()
+# link pose files
+list_arm = open("./poses/arms.txt").readlines() # reads arms.txt in poses and turns it into a list called list_arm
 list_chest = open(".poses/chest.txt").readlines()
 list_legs = open(".poses/legs.txt").readlines()
 list_back = open(".poses/back.txt").readlines()
 
+# display menu
 def display_menu(menu):
     for k, function in menu.items():
         print(k, function.__name__)
 
-def Arms():
-    print(random.choice(list_arm))
+# arms
+def Arms(): # creates a function called 'Arms'
+    print(random.choice(list_arm)) # picks a random item from list_arm
 
+# chest
 def Chest():
     print(random.choice(list_chest))
 
+# legs
 def Legs():
     print(random.choice(list_legs))
 
+# back
 def Back():
     print(random.choice(list_back))
 
+# exit program
 def Done():
-    print("------GOODBYE-----")
+    print("GOODBYE")
     sys.exit()
 
+# menu
 def main():
-    os.system("clear")
-    functions_names = [Arms, Chest, Legs, Back, Done]
-    menu_items = dict(enumerate(functions_names, start=1))
+    os.system("clear") # calls the system command "clear" to clear the screen
+    functions_names = [Arms, Chest, Legs, Back, Done] # creates a list from the functions defined above
+    menu_items = dict(enumerate(functions_names, start=1)) # enumerates the functions called in functions_names and puts them in a new list called menu_items
 
     while True:
-        print('-------YOGA-------')
+        print('------------------') # creates a super cool banner
         display_menu(menu_items)
         print('------------------')
         selection = int(
